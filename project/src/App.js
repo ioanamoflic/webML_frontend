7c83ac6ab3ab389fe36e6a058c1cde209d2f3c4a
Miru19 <56609347+Miru19@users.noreply.github.com>
2021-10-21 19:55:49 +0300

    Add context

--- project/src/App.js
+++ project/src/App.js
@@ -2,12 +2,21 @@ import './App.css';
 import Login from './Pages/Login';
 import TablePage from './Pages/TablePage';
 import ImagePage from './Pages/ImagePage'
+import {AuthContext} from './Pages/AuthContext'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
 function App() {
+
+  const UserComponents = () => {
+    <AuthContext.Provider>
+        <Login/>
+        <TablePage></TablePage>
+        <ImagePage></ImagePage>
+    </AuthContext.Provider>
+}
   return (
     <Router>
       <Switch>
