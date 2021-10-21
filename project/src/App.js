51cbf8bec6896a7a1d7cd101460612941108d2d8
Miru19 <56609347+Miru19@users.noreply.github.com>
2021-10-21 16:52:28 +0300

    Implement functionality for login

--- project/src/App.js
+++ project/src/App.js
@@ -6,39 +6,15 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 function App() {
   return (
     <Router>
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/table">Table</Link>
-          </li>
-          <li>
-            <Link to="/upload">Upload</Link>
-          </li>
-        </ul>
-      </nav>
-
-
       <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/table">
-          <TablePage />
-        </Route>
-        <Route path="/upload">
-         <ImagePage></ImagePage>
-        </Route>
+        <Route exact path="/" component={Login}/>
+        <Route path="/table" component={TablePage}/>
+        <Route path="/upload" component={ImagePage}/>
       </Switch>
-    </div>
   </Router>
   );
 }
--- project/src/Pages/Login.js
+++ project/src/Pages/Login.js
@@ -1,34 +1,69 @@
 import { Button, TextField, Link, Grid, Paper, Box } from '@mui/material';
+import { Component } from 'react';
+import loginImg from '../Photos/LoginPhoto.gif';
 
-function Login() {
-  return (
-    <Box sx={{ m: 20 }}>
-      <Grid container spacing={2} >
-        <Grid item xs={7}>
-          <Paper elevation="1">
-            <img src={require('../Photos')} alt="ana" style={{ height: 500, width: 500, border: "2px solid black" }} />
-          </Paper>
-        </Grid>
-        <Grid item xs={5}>
-          <Paper sx={{ display: 'flex', flexDirection: 'column'}} elevation="0">
-            <TextField
-              id="outlined-input"
-              label="Email ID"
-              type="text"
-            />&nbsp;
-            <TextField
-              id="outlined-password-input"
-              label="Password"
-              type="password"
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: null,
+      password: null, 
+    };
 
-            />&nbsp;
-            <Button fullWidth="true" Color="Color.Primary" variant="contained">Login</Button>&nbsp;
-            <Link href="#" underline="none">Forgot Username/Password?</Link>
-          </Paper>
+    this.buttonClicked = this.buttonClicked.bind(this);
+    this.onEmailChange = this.onEmailChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+  }
+
+
+  onEmailChange(event) {
+    this.setState({ email: event.target.value })
+  }
+
+  onPasswordChange(event) {
+    this.setState({ password: event.target.value })
+  }
+
+  buttonClicked = ()=> {
+    this.props.history.push('/table');
+  }
+
+  render() {
+    return (
+
+      <Box>
+        <Grid container spacing={1}>
+          <Grid item xs={7} >
+            <Paper elevation="0">
+              <img src={loginImg} alt="Robot" style={{ width: '50%', height: 'auto' }} />
+            </Paper>
+          </Grid>
+          <Grid item xs={5}>
+            <Paper elevation="0">
+              <TextField
+                id="outlined-input"
+                label="Email ID"
+                type="text"
+                fullWidth="true"
+                value={this.state.email}
+                onChange={this.onEmailChange}
+              />&nbsp;
+              <TextField
+                id="outlined-password-input"
+                label="Password"
+                type="password"
+                fullWidth="true"
+                value={this.state.password}
+                onChange={this.onPasswordChange}
+              />&nbsp;
+              <Button fullWidth="true" Color="Color.Primary" variant="contained" onClick={this.buttonClicked}>Login</Button>&nbsp;
+              <Link href="#" underline="none">Forgot Username/Password?</Link>
+            </Paper>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
-  );
+      </Box>
+    );
+  }
 }
 
 export default Login;
